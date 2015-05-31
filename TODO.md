ecd41c2c0d6b79ad39f4a25f8516ce97d77be6f7
Konrad Świć <konrad@forumakad.pl>
2015-05-31 23:53:38 +0200

    Fix empty document; other filetypes; prototype of drag indication

---
+++ TODO.md
@@ -0,0 +1,9 @@
+- spinner / loading indicator
+- drop target indication
+ - instead of child element use class and ::before
+ - with pos:abs we might not need special event handling for dragleave
+<!--  do nothing if not dropped from outside  -->
+<!-- - fix problem with empty document -->
+<!-- - handling of files other than images -->
+   <!-- (postponed - backend doesn't support this) -->
+- progress meter if possible
--- plugin.js
+++ plugin.js
@@ -1,37 +1,81 @@
 CKEDITOR.plugins.add('droploader', {
 	init: function (editor) {
-
+		var pluginDirectory = this.path;
+		editor.addContentsCss(pluginDirectory + 'styles/droploader.css');
+		function handleUpload(url) {
+			if (isImg.test(url)) {
+				var img = new CKEDITOR.dom.element('img');
+				img.setAttribute('src', url);
+				editor.insertElement(img);
+			} else if (isAnotherSupportedType.test(url)) {
+				var link = new CKEDITOR.dom.element('a');
+				link.setAttribute('href', url);
+				link.textContents = url;
+				editor.insertElement(link);
+			}
+		}
+		var isImg = /.*\.(png|jpe?g|gif)$/;
+		var isAnotherSupportedType = /\.(pdf)$/;
 		editor.on('contentDom', function () {
+			editor.document.on('dragover', function(e) {
+				if (e.data.$.dataTransfer.types.indexOf('Files') > -1) {
+					e.data.$.preventDefault();
+				}
+			});
+			var dragEventCounter = 0;
+			editor.document.on('dragenter', function(e) {
+				// e.data.stopPropagation();
+				console.log(dragEventCounter, e.data.$.target, e.data.$.relatedTarget);
+				if (dragEventCounter == 0) {
+					var marker = new CKEDITOR.dom.element('div');
+					marker.setAttribute('id', 'drop_marker')
+					.setText('Drop files here to upload');
+					editor.insertElement(marker);
+				};
+				dragEventCounter++;
+			});
+			editor.document.on('dragleave', function(e) {
+				// e.data.stopPropagation();
+				dragEventCounter--;
+				console.log(dragEventCounter, e.data.$.target, e.data.$.relatedTarget);
+				if (dragEventCounter == 0) {
+					editor.document.getById('drop_marker').remove();
+
+				}
+			})
 			editor.document.on('drop', function (e) {
+
+				editor.document.getById('drop_marker').remove();
 				// Prepare dropped file for upload
 				if (e.data.$.dataTransfer.files.length > 0) {
 					e.data.$.preventDefault();
-					debugger;
 					var files = e.data.$.dataTransfer.files;
 					var fd = new FormData();
+				    var someFilesToUpload = false;
 					for (var i=0; i<files.length; i++) {
-						if (/.*\.(png|jpg|gif)$/.test(files[i].name)) {
+						if (isImg.test(files[i].name) || 
+							isAnotherSupportedType.test(files[i].name)) {
 							fd.append('upload', files[i]);
+							someFilesToUpload = true;
 						}
 					}
 					// set an upload handler via CKEDITOR.tools
-					fileUploadedHandler = CKEDITOR.tools.addFunction(function(url) {
-						var img = new CKEDITOR.dom.element('img');
-						img.setAttribute('src', url);
-						editor.insertElement(img);
-					});
-					var xhr = new XMLHttpRequest();
-					xhr.open('POST', editor.config.filebrowserUploadUrl + 
-						'?CKEditorFuncNum=' + fileUploadedHandler);
-					xhr.responseType = 'document';
-					xhr.onload = function (e) {
-						if (this.status == 200) {
+					if (someFilesToUpload) {
+						fileUploadedHandler = CKEDITOR.tools.addFunction(handleUpload);
+						var xhr = new XMLHttpRequest();
+						xhr.open('POST', editor.config.filebrowserUploadUrl + 
+							'?CKEditorFuncNum=' + fileUploadedHandler);
+						xhr.responseType = 'document';
+						xhr.onload = function (e) {
+							if (this.status == 200) {
 							// hack to run the callback (meant to be opened in pop-up window?)
 							// eval(this.response.getElementsByTagName('script')[0].innerHTML);
-							new Function("", this.response.getElementsByTagName('script')[0].innerHTML)();
+							var responseCallback = new Function("", this.response.getElementsByTagName('script')[0].innerHTML);
+							responseCallback();
+							}
 						}
-					}
-					xhr.send(fd);
+						xhr.send(fd);
+					}					
 				}
 			});
 			
